41418c49d1271af23d55e512b2b631f8340aa097
Marcos Gomes <marcosgomesdev@gmail.com>
2024-08-06 16:24:00 -0300

    style: add shadow bottom

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className="h-20 w-full items-center flex bg-white sticky top-0 z-50 border-b-[0.1px] border-blue700">
+    <div className="h-20 w-full items-center flex bg-[#ededed] shadow-bottom sticky top-0 z-50">
       <div className="container">
         <h1 className="text-2xl font-semibold poppins cursor-default">
           Leonardo Jardim
